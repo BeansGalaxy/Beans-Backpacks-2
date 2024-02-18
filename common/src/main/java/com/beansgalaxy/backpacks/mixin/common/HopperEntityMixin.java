1177b1b1ebcbe3e3ab7022d416adbbef5f6ac3d6
Beans Galaxy <connerarsenault@gmail.com>
2024-02-18 00:06:10 -0500

    Fixed forge crashing on launch

--- common/src/main/java/com/beansgalaxy/backpacks/mixin/common/HopperEntityMixin.java
+++ common/src/main/java/com/beansgalaxy/backpacks/mixin/common/HopperEntityMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.Hopper;
 import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -27,40 +28,16 @@ public class HopperEntityMixin {
 
     @Inject(method = "getContainerAt(Lnet/minecraft/world/level/Level;DDD)Lnet/minecraft/world/Container;",
             at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private static void getContainerAt(Level $$0, double $$1, double $$2, double $$3, CallbackInfoReturnable<Container> cir, Container container) {
+    private static void getContainerAt(Level level, double x, double y, double z, CallbackInfoReturnable<Container> cir, Container container) {
         if (container == null) {
-            List<Entity> $$9 = $$0.getEntities((Entity)null, new AABB($$1 - 0.5, $$2 - 0.5, $$3 - 0.5, $$1 + 0.5, $$2 + 0.5, $$3 + 0.5), in -> in instanceof BackpackEntity);
+            List<Entity> $$9 = level.getEntities((Entity)null, new AABB(x - 0.5, y - 0.5, z - 0.5, x + 0.5, y + 0.5, z + 0.5), in -> in instanceof BackpackEntity);
             if (!$$9.isEmpty()) {
-                BackpackEntity backpackEntity = (BackpackEntity) $$9.get($$0.random.nextInt($$9.size()));
+                BackpackEntity backpackEntity = (BackpackEntity) $$9.get(level.random.nextInt($$9.size()));
                 cir.setReturnValue(backpackEntity.backpackInventory);
             }
         }
     }
 
-    @Shadow
-    private static boolean isEmptyContainer(Container $$0, Direction $$1) {
-        return false;
-    }
-
-    @Inject(method = "suckInItems", locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true,
-            at = @At(value = "FIELD", target = "Lnet/minecraft/core/Direction;DOWN:Lnet/minecraft/core/Direction;"))
-    private static void tryTakeFromBackpack(Level level, Hopper hopper, CallbackInfoReturnable<Boolean> cir, Container container) {
-        if (container instanceof BackpackInventory backpackInventory) {
-            Direction direction = Direction.DOWN;
-            boolean r = !isEmptyContainer(container, direction) && backpackInventory.hopperTakeOne(hopper);
-            if (r)
-                PlaySound.TAKE.at(backpackInventory.getOwner(), backpackInventory.getLocalData().kind(), 0.8f);
-            cir.setReturnValue(r);
-        }
-    }
-    @Inject(method = "ejectItems", locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true,
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/Container;getContainerSize()I"))
-    private static void tryEjectToBackpack(Level $$0, BlockPos $$1, BlockState $$2, Container hopper, CallbackInfoReturnable<Boolean> cir, Container container, Direction $$5, int $$6) {
-        if (container instanceof BackpackInventory backpackInventory) {
-            boolean r = backpackInventory.hopperInsertOne(hopper);
-            cir.setReturnValue(r);
-        }
-    }
     @Inject(method = "getSlots", cancellable = true, at = @At("HEAD"))
     private static void addBackpackSlotsCheck(Container container, Direction $$1, CallbackInfoReturnable<IntStream> cir) {
         if (container instanceof BackpackInventory backpackInventory && backpackInventory.isEmpty()) {
