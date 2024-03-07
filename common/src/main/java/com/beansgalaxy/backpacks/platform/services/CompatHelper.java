d7bb889a44978647e94455259c6a1e77af103308
Beans Galaxy <connerarsenault@gmail.com>
2024-03-07 18:59:24 -0500

    Added method for grave mods to grab when a player dies.

--- common/src/main/java/com/beansgalaxy/backpacks/platform/services/CompatHelper.java
+++ common/src/main/java/com/beansgalaxy/backpacks/platform/services/CompatHelper.java
@@ -1,6 +1,8 @@
 package com.beansgalaxy.backpacks.platform.services;
 
 import com.beansgalaxy.backpacks.data.BackData;
+import net.minecraft.core.Direction;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 
@@ -19,6 +21,10 @@ public interface CompatHelper {
 
       default boolean graveModLoaded() {
             return anyModsLoaded(new String[]{"universal-graves", "yigd", "gravestones"});
+      }
+
+      static NonNullList<ItemStack> onDeathForGraveMods(Player player, int x, double y, int z, Direction direction, float yaw) {
+            return BackData.get(player).drop(x, y, z, direction, yaw);
       }
 
       void setBackSlotItem(BackData data, ItemStack stack);
